e7dd6eff59485f941dff0782dabd52c741b668a6
Ivan Malagon <hacheka@gmail.com>
2022-02-09 16:37:11 +0100

    adds tests

--- src/index.js
+++ src/index.js
@@ -224,14 +224,16 @@ var tracker = function (w) {
 
   function getCookie(name) {
     var nameEQ = name + '='
-    var ca = w.document.cookie.split(';')
-    for (var i = 0; i < ca.length; ++i) {
-      var c = ca[i]
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1, c.length)
-      }
-      if (c.indexOf(nameEQ) === 0) {
-        return c.substring(nameEQ.length, c.length)
+    if (w.document.cookie) {
+      var ca = w.document.cookie.split(';')
+      for (var i = 0; i < ca.length; ++i) {
+        var c = ca[i]
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1, c.length)
+        }
+        if (c.indexOf(nameEQ) === 0) {
+          return c.substring(nameEQ.length, c.length)
+        }
       }
     }
     return null
